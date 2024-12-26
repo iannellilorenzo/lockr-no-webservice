d4970dc4cda0d4753ea73a8e09a3b27ec57b366f
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-12-26 18:16:34 +0100

    konscious library added for argon2id hash function for storing password, secret key, and verification token. xml docs added

--- lockr-no-webservice/User.cs
+++ lockr-no-webservice/User.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Konscious.Security.Cryptography;
 
 namespace lockr_no_webservice
 {
@@ -27,7 +29,9 @@ namespace lockr_no_webservice
         private int _statusId;
         private int _roleId;
 
-        // Properties with getter and setter
+        /// <summary>
+        /// Gets or sets the email of the user.
+        /// </summary>
         public string Email
         {
             get => _email;
@@ -35,7 +39,7 @@ namespace lockr_no_webservice
             {
                 try
                 {
-                    var mailAddress = new MailAddress(value);
+                    var mailAddress = new System.Net.Mail.MailAddress(value);
                     _email = mailAddress.Address;
                 }
                 catch (FormatException)
@@ -45,6 +49,9 @@ namespace lockr_no_webservice
             }
         }
 
+        /// <summary>
+        /// Gets or sets the username of the user.
+        /// </summary>
         public string Username
         {
             get => _username;
@@ -61,6 +68,9 @@ namespace lockr_no_webservice
             }
         }
 
+        /// <summary>
+        /// Gets or sets the first name of the user.
+        /// </summary>
         public string FirstName
         {
             get => _firstName;
@@ -77,6 +87,9 @@ namespace lockr_no_webservice
             }
         }
 
+        /// <summary>
+        /// Gets or sets the last name of the user.
+        /// </summary>
         public string LastName
         {
             get => _lastName;
@@ -93,6 +106,9 @@ namespace lockr_no_webservice
             }
         }
 
+        /// <summary>
+        /// Gets or sets the password hash of the user.
+        /// </summary>
         public string PasswordHash
         {
             get => _passwordHash;
@@ -100,7 +116,7 @@ namespace lockr_no_webservice
             {
                 if (Regex.IsMatch(value, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$"))
                 {
-                    _passwordHash = value;
+                    _passwordHash = Argon2idHash(value);
                 }
                 else
                 {
@@ -109,6 +125,9 @@ namespace lockr_no_webservice
             }
         }
 
+        /// <summary>
+        /// Gets or sets the phone number of the user.
+        /// </summary>
         public string PhoneNumber
         {
             get => _phoneNumber;
@@ -125,42 +144,73 @@ namespace lockr_no_webservice
             }
         }
 
+        /// <summary>
+        /// Gets or sets the creation date of the user.
+        /// </summary>
         public DateTime CreatedAt
         {
             get => _createdAt;
             set => _createdAt = value;
         }
 
+        /// <summary>
+        /// Gets or sets the last update date of the user.
+        /// </summary>
         public DateTime UpdatedAt
         {
             get => _updatedAt;
             set => _updatedAt = value;
         }
 
+        /// <summary>
+        /// Gets or sets the secret key of the user.
+        /// </summary>
         public string SecretKey
         {
             get => _secretKey;
-            set => _secretKey = value;
+            set
+            {
+                if (Regex.IsMatch(value, @"^\d{6}$"))
+                {
+                    _secretKey = Argon2idHash(value);
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid secret key format.");
+                }
+            }
         }
 
+        /// <summary>
+        /// Gets or sets the verification token of the user.
+        /// </summary>
         public string VerificationToken
         {
             get => _verificationToken;
-            set => _verificationToken = value;
+            set => _verificationToken = Argon2idHash(value);
         }
 
+        /// <summary>
+        /// Gets or sets the status ID of the user.
+        /// </summary>
         public int StatusId
         {
             get => _statusId;
             set => _statusId = value;
         }
 
+        /// <summary>
+        /// Gets or sets the role ID of the user.
+        /// </summary>
         public int RoleId
         {
             get => _roleId;
             set => _roleId = value;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class.
+        /// </summary>
         public User()
         {
             Email = "default@example.com";
@@ -177,6 +227,21 @@ namespace lockr_no_webservice
             RoleId = 0;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class with specified parameters.
+        /// </summary>
+        /// <param name="email">The email of the user.</param>
+        /// <param name="username">The username of the user.</param>
+        /// <param name="firstName">The first name of the user.</param>
+        /// <param name="lastName">The last name of the user.</param>
+        /// <param name="passwordHash">The password hash of the user.</param>
+        /// <param name="phoneNumber">The phone number of the user.</param>
+        /// <param name="createdAt">The creation date of the user.</param>
+        /// <param name="updatedAt">The last update date of the user.</param>
+        /// <param name="secretKey">The secret key of the user.</param>
+        /// <param name="verificationToken">The verification token of the user.</param>
+        /// <param name="statusId">The status ID of the user.</param>
+        /// <param name="roleId">The role ID of the user.</param>
         public User(string email, string username, string firstName, string lastName, string passwordHash, string phoneNumber, DateTime createdAt, DateTime updatedAt, string secretKey, string verificationToken, int statusId, int roleId)
         {
             Email = email;
@@ -193,6 +258,10 @@ namespace lockr_no_webservice
             RoleId = roleId;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class by copying another user.
+        /// </summary>
+        /// <param name="user">The user to copy.</param>
         public User(User user)
         {
             Email = user.Email;
@@ -210,12 +279,44 @@ namespace lockr_no_webservice
         }
 
         /// <summary>
+        /// Generates a cryptographically secure salt.
+        /// </summary>
+        /// <returns>A byte array for salt.</returns>
+        private byte[] GenerateSalt()
+        {
+            byte[] saltBytes = new byte[16];
+
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(saltBytes);
+            }
+
+            return saltBytes;
+        }
+
+        /// <summary>
+        /// Hashes the given password using the Argon2id algorithm.
+        /// </summary>
+        /// <param name="password">The password to hash.</param>
+        /// <returns>The hashed password as a base64 string.</returns>
+        private string Argon2idHash(string password)
+        {
+            var argon2id = new Argon2id(Encoding.UTF8.GetBytes(password));
+            argon2id.Salt = GenerateSalt();
+            argon2id.DegreeOfParallelism = 8; // Four cores
+            argon2id.MemorySize = 1024 * 1024; // 1 GB
+            argon2id.Iterations = 4;
+
+            return Convert.ToBase64String(argon2id.GetBytes(16));
+        }
+
+        /// <summary>
         /// Returns a string that represents the current object.
         /// </summary>
         /// <returns>A string that represents the current object.</returns>
         public override string ToString()
         {
-            return $"User: {Username}, Email: {Email}, Name: {FirstName} {LastName}, Phone: {PhoneNumber}, CreatedAt: {CreatedAt}, UpdatedAt: {UpdatedAt}, StatusId: {StatusId}, RoleId: {RoleId}";
+            return $"User: {Username}, Email: {Email}, Phone: {PhoneNumber}, CreatedAt: {CreatedAt}, UpdatedAt: {UpdatedAt}";
         }
 
         /// <summary>
@@ -224,21 +325,7 @@ namespace lockr_no_webservice
         /// <returns>A copy of the current object.</returns>
         public User Copy()
         {
-            return new User
-            {
-                Email = this.Email,
-                Username = this.Username,
-                FirstName = this.FirstName,
-                LastName = this.LastName,
-                PasswordHash = this.PasswordHash,
-                PhoneNumber = this.PhoneNumber,
-                CreatedAt = this.CreatedAt,
-                UpdatedAt = this.UpdatedAt,
-                SecretKey = this.SecretKey,
-                VerificationToken = this.VerificationToken,
-                StatusId = this.StatusId,
-                RoleId = this.RoleId
-            };
+            return new User(this);
         }
 
         /// <summary>
