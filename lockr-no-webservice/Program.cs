4d9247d4e9feef8eb103da25b88b91c5667a2c65
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-12-26 18:26:33 +0100

     

--- lockr-no-webservice/Program.cs
+++ lockr-no-webservice/Program.cs
@@ -16,7 +16,7 @@ namespace lockr_no_webservice
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
