2225bf198a69ca8ccbbcca91b4bf738b01caa57e
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-12-26 16:51:07 +0100

    some methods for establishing connection and execute querys

---
+++ lockr-no-webservice/DatabaseHelper.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lockr_no_webservice
+{
+    public class DatabaseHelper
+    {
+        private string connectionString = "Server=localhost;Database=account_manager;User=root;Password=;";
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        public void ExecuteNonQuery(string query, Dictionary<string, object> parameters)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    foreach (var param in parameters)
+                    {
+                        command.Parameters.AddWithValue(param.Key, param.Value);
+                    }
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public MySqlDataReader ExecuteQuery(string query, Dictionary<string, object> parameters)
+        {
+            var connection = GetConnection();
+            connection.Open();
+            using (var command = new MySqlCommand(query, connection))
+            {
+                foreach (var param in parameters)
+                {
+                    command.Parameters.AddWithValue(param.Key, param.Value);
+                }
+                return command.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+            }
+        }
+    }
+}
